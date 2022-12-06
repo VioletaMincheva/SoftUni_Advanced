1f8f9363c120c98617c0f497e8f79d6c496481de
Violeta Mincheva <118520945+VioletaMincheva@users.noreply.github.com>
2022-12-06 12:20:27 +0200

    Add files via upload

---
+++ Generics/GenericSwapMethodStrings_ex_3/Box.java
@@ -0,0 +1,33 @@
+package JavaAdvanced.Generics.GenericSwapMethodStrings_ex_3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Box<T> {
+    public List<T> values;
+
+    public Box() {
+        this.values = new ArrayList<>();
+    }
+
+    public void addValue(T value) {
+        this.values.add(value);
+    }
+
+    public void swapValues(int index1, int index2) {
+        T firstValue = values.get(index1);
+        values.set(index1, this.values.get(index2));
+        values.set(index2, firstValue);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder output = new StringBuilder();
+        for (T value : values) {
+            output.append(String.format("%s: %s", value.getClass().getName(), value))
+                    .append(System.lineSeparator());
+        }
+        return output.toString().trim();
+    }
+}
+
